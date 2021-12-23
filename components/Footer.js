00b154eb85431b902d1f5610684c29a6862cd635
lornawanjiru <lornamuchangi@gmail.com>
2021-12-23 12:27:52 -0800

    first task

---
+++ components/Footer.js
@@ -0,0 +1,13 @@
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+
+function footers(){
+ return(
+    <footer className={styles.footer}>
+        <p>
+          Designed and Developed by Lorna Wanjiru
+        </p>  
+      </footer>
+ )
+}
+export default footers
---
+++ components/Nav.js
@@ -0,0 +1,6 @@
+function nav(){
+    return(
+        <h1>Nav</h1> 
+     )
+    }
+    export default nav
