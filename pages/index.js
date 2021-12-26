7f0c977f46eaac200a7284dc81397621f99016cc
Lorna <lorna@localhost.localdomain>
2021-12-26 13:32:55 +0300

    homepagecomplete

--- pages/index.js
+++ pages/index.js
@@ -75,27 +75,27 @@ export default function Home() {
            <div className={styles.grid}>
              <div className={styles.cards}>
              <div className={styles.card}>
-               <Image src="/3.jpg" height={300} width={400}/>
+               
                 </div>
                 <h2> Events photography</h2>
              </div>
              <div className={styles.cards}>
-             <div className={styles.card}>
-             <Image src="/4.jpg" height={300} width={400}/>
+             <div className={styles.card1}>
+             
             </div>
             <h2> Drone Shoots </h2>
              </div>
             </div>
             <div className={styles.grid}>
              <div className={styles.cards}>
-             <div className={styles.card}>
-             <Image src="/5.jpg" height={300} width={400}/>
+             <div className={styles.card2}>
+            
                 </div>
                 <h2> Advertisement</h2>
              </div>
              <div className={styles.cards}>
-             <div className={styles.card}>
-             <Image src="/6.jpg" height={300} width={400}/>
+             <div className={styles.card3}>
+            
             </div>
             <h2> Wallpapers </h2>
              </div>
