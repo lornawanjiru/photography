4421fb46033b8a681f736ad6fbd7c0ffa4721a7a
Lorna <lorna@localhost.localdomain>
2021-12-26 14:52:16 +0300

    resumepagecomplete

--- pages/resume.js
+++ pages/resume.js
@@ -1,11 +1,81 @@
 import styles from '../styles/Home.module.css';
 
 function Resume(){
+         
  return(
-    <div className={styles.footer}>
-        <h3> My resume </h3>
-        
-      </div>
+    <div className={styles.container}>
+      <header>
+          <h1 className={styles.title}>
+            My Resume
+          </h1>
+          <hr className={styles.hr}/>
+          <p className={styles.description}>
+            Hello there,
+            <br/>
+           Just showcasing my skills
+          </p>
+        </header>
+      <section className={styles.timelinesection}>
+        <div className={styles.timelineitems}>
+          <div className={styles.timelineitem}>
+            <div className={styles.timelinedot}></div>
+            <div className={styles.timelinedate}>2015</div>
+            <div className={styles.timelinecontent}>
+              <h3>timeline item title</h3>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
+            </div>
+          </div>
+          <div className={styles.timelineitem}>
+            <div className={styles.timelinedot}></div>
+            <div className={styles.timelinedate}>2015</div>
+            <div className={styles.timelinecontent}>
+              <h3>timeline item title</h3>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
+            </div>
+          </div>
+          <div className={styles.timelineitem}>
+            <div className={styles.timelinedot}></div>
+            <div className={styles.timelinedate}>2015</div>
+            <div className={styles.timelinecontent}>
+              <h3>timeline item title</h3>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
+            </div>
+          </div>
+          <div className={styles.timelineitem}>
+            <div className={styles.timelinedot}></div>
+            <div className={styles.timelinedate}>2015</div>
+            <div className={styles.timelinecontent}>
+              <h3>timeline item title</h3>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
+            </div>
+          </div>
+          <div className={styles.timelineitem}>
+            <div className={styles.timelinedot}></div>
+            <div className={styles.timelinedate}>2015</div>
+            <div className={styles.timelinecontent}>
+              <h3>timeline item title</h3>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
+            </div>
+          </div>
+          <div className={styles.timelineitem}>
+            <div className={styles.timelinedot}></div>
+            <div className={styles.timelinedate}>2015</div>
+            <div className={styles.timelinecontent}>
+              <h3>timeline item title</h3>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
+            </div>
+          </div>
+          <div className={styles.timelineitem}>
+            <div className={styles.timelinedot}></div>
+            <div className={styles.timelinedate}>2015</div>
+            <div className={styles.timelinecontent}>
+              <h3>timeline item title</h3>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
+            </div>
+          </div>
+        </div>
+      </section>
+    </div>
  )
 }
 export default Resume
